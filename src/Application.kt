bad2db36a0ab0875ec1241cca5a8e4a078af1e52
Peter-John Welcome <pjwelcome@gmail.com>
2019-07-16 12:03:17 +0200

    Updated the project with workshop changes

--- src/Application.kt
+++ src/Application.kt
@@ -1,21 +1,53 @@
 package com.example
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
-import io.ktor.features.*
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.basic
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.jackson.jackson
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import java.util.*
+
+data class Snippet(val text: String)
+
+val snippets = Collections.synchronizedList(
+    mutableListOf(
+        Snippet("hello"),
+        Snippet("world")
+    )
+)
+
+data class PostSnippet(val snippet: PostSnippet.Text) {
+    data class Text(val text: String)
+}
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+
     install(Authentication) {
+        basic {
+            realm = "myrealm"
+            validate { if (it.name == "user" && it.password == "password") UserIdPrincipal("user") else null }
+        }
     }
 
     install(ContentNegotiation) {
@@ -36,12 +68,28 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
+
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
         get("/json/jackson") {
             call.respond(mapOf("hello" to "world"))
+        }
+
+        route("/snippets") {
+
+            get {
+                call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+            }
+            authenticate {
+                post {
+                    val post = call.receive<PostSnippet>()
+                    snippets += Snippet(post.snippet.text)
+                    call.respond(mapOf("OK" to true))
+                }
+
+            }
         }
     }
 }
